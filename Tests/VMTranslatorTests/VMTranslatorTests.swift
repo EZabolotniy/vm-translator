0e38922a1cc0881a352fd7fd3ae6c9ebe1ef367b
Evgeniy Zabolotniy <eugenezabolotniy@yandex.ru>
2023-04-04 13:08:10 +0800

    Initial Commit

---
+++ Tests/VMTranslatorTests/VMTranslatorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VMTranslator
+
+final class VMTranslatorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VMTranslator().text, "Hello, World!")
+    }
+}
